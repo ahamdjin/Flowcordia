a633e896d66e48998dbd88325c34a35953d132f8
Ahmad <ahamdjin34@gmail.com>
2026-07-21 04:28:29 +0500

    Make bootstrap commands JSON-submittable

--- apps/webapp/app/features/flowcordia/workflows/bootstrap/command-contract.ts
+++ apps/webapp/app/features/flowcordia/workflows/bootstrap/command-contract.ts
@@ -1,12 +1,12 @@
 export const FLOWCORDIA_BOOTSTRAP_CONFIRMATION = "CREATE_FLOWCORDIA_STARTER_WORKFLOW";
 
-export interface FlowcordiaBootstrapCommand {
+export type FlowcordiaBootstrapCommand = {
   operation: "bootstrap";
   confirmation: typeof FLOWCORDIA_BOOTSTRAP_CONFIRMATION;
   workflowId: string;
   name: string;
   description: string;
-}
+};
 
 export function buildFlowcordiaBootstrapCommand(input: {
   workflowId: string;
