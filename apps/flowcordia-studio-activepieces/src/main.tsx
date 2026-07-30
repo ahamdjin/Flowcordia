aff51a98bd5323140becec188a7df35e2c53bb01
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-07-31 03:26:48 +0500

    Boot the Flowcordia Activepieces Studio host

---
+++ apps/flowcordia-studio-activepieces/src/main.tsx
@@ -0,0 +1,17 @@
+import "@/polyfills";
+import "@/i18n";
+import "@/index.css";
+import "@xyflow/react/dist/style.css";
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { FlowcordiaActivepiecesStudioHost } from "./studio-host";
+import "./studio-host.css";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("Flowcordia Studio root element is missing.");
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <FlowcordiaActivepiecesStudioHost />
+  </React.StrictMode>
+);
